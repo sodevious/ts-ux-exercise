c7b7eff1f21d3e2bb45952a51753edd1df7cf3e6
Nicole Dominguez <sodevious.net@gmail.com>
2020-02-04 21:55:44 -0800

    button component build

---
+++ src/components/Button.vue
@@ -0,0 +1,51 @@
+<template>
+  <button class="ts-button">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button"
+};
+</script>
+
+<style lang="scss">
+@mixin ts-button-theme($background, $color) {
+  background-color: $background;
+  color: $color;
+
+  &:hover {
+    color: darken($color, 15%);
+    background-color: darken($background, 5%);
+  }
+}
+
+.ts-button {
+  @include ts-button-theme($ts-gray-tint-4, $ts-black-toned);
+  -webkit-appearance: button;
+  text-decoration: none;
+  box-sizing: border-box;
+  cursor: pointer;
+  border: none;
+  transition: 0.25s all ease-out;
+  border-radius: $ts-button-size / 2;
+  padding: 0 ($ts-gutter * 2);
+  height: $ts-button-size;
+  line-height: $ts-button-size;
+  font-size: $ts-font-small;
+
+
+  & + & {
+    margin-right: 1rem;
+  }
+}
+
+.ts-button-link {
+  @include ts-button-theme($ts-white, $ts-blue-base);
+}
+
+.ts-button-primary {
+  @include ts-button-theme($ts-blue-base, $ts-white);
+}
+</style>
