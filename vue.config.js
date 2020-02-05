08d14701d971f5aa99ab019b8a588194cc215a69
Nicole Dominguez <sodevious.net@gmail.com>
2020-02-04 21:54:00 -0800

    base styles for app

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: `@import "~@/scss/_variables.scss";`
+      },
+    }
+  }
 };
